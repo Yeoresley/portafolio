291f8da15e394efe208ece2d886ce2c8e9107ff8
Yeoresley <58912638+Yeoresley@users.noreply.github.com>
2025-11-06 15:55:41 -0500

    Delete next.config.ts

--- next.config.ts
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: { appDir: true },
-  i18n: { locales: ['es','en'], defaultLocale: 'es' },
-};
-export default nextConfig;
